8441ba8b82bac953f766c4897a44c2ea6b9e8dce
Cem Gurhan <cemg@live.co.uk>
2022-05-30 13:09:29 +0100

    Q2 done

--- code/arrays.js
+++ code/arrays.js
@@ -19,7 +19,8 @@ console.log(stations);
  * It doesn't go as far as Dundee either, so remove it from the start of the array.
  */
 
-// console.log(stations);
+stations.shift("Dundee");
+console.log(stations);
 
 
 
